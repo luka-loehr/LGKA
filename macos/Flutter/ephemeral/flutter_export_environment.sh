f79af51545ac5646b1fe059c31b41da2b425bc60
Luka Löhr <ycrafty6@gmail.com>
2025-08-19 15:42:53 +0200

    prepare for ios

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/luka/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/luka/Documents/LGKA+/lgka_flutter"
+export "FLUTTER_ROOT=/Users/Luka/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Luka/Documents/LGKA"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.0.4"
-export "FLUTTER_BUILD_NUMBER=40"
+export "FLUTTER_BUILD_NAME=2.0.1"
+export "FLUTTER_BUILD_NUMBER=28"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
