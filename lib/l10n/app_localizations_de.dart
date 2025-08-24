479ba140c874c401e6890ceb3cc5b7e8edf98dd6
Luka Löhr <ycrafty6@gmail.com>
2025-08-24 13:35:53 +0200

    Set German as primary language for app store metadata

---
+++ lib/l10n/app_localizations_de.dart
@@ -0,0 +1,31 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for German (`de`).
+class AppLocalizationsDe extends AppLocalizations {
+  AppLocalizationsDe([String locale = 'de']) : super(locale);
+
+  @override
+  String get appTitle => 'LGKA+';
+
+  @override
+  String get welcomeTitle => 'Willkommen bei LGKA+';
+
+  @override
+  String get substitutionPlan => 'Vertretungsplan';
+
+  @override
+  String get weather => 'Wetter';
+
+  @override
+  String get schedule => 'Stundenplan';
+
+  @override
+  String get legal => 'Impressum';
+
+  @override
+  String get privacy => 'Datenschutz';
+}
