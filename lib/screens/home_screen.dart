cf15ac31a27502b43c0614142cd08b3b4563373e
Luka Löhr <ycrafty6@gmail.com>
2025-09-19 16:22:51 +0200

    Use school building icon for leading button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -78,7 +78,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
       leading: IconButton(
         onPressed: _onMedkitPressed,
         icon: const Icon(
-          Icons.school_outlined,
+          Icons.domain,
           color: AppColors.secondaryText,
         ),
       ),
