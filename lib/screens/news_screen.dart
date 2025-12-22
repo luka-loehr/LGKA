c1ce2ee6fc4a576c157cbdaeae8e2e63c5900825
Luka Löhr <ycrafty6@gmail.com>
2025-12-22 17:17:08 +0100

    Set news cards fade-in animation to 2 seconds

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -27,7 +27,7 @@ class _NewsScreenState extends ConsumerState<NewsScreen> with TickerProviderStat
   void initState() {
     super.initState();
     _listAnimationController = AnimationController(
-      duration: const Duration(milliseconds: 3000),
+      duration: const Duration(milliseconds: 2000),
       vsync: this,
     );
 
