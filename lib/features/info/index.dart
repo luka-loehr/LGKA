cd6ae39a3dc8149703839447fb39f3a2da92cdc6
Luka Löhr <luka@lukaloehr.com>
2026-01-04 15:41:34 +0100

    Fix all info-level style suggestions

--- lib/features/info/index.dart
+++ lib/features/info/index.dart
@@ -1,5 +1,4 @@
 // Copyright Luka Löhr 2025
 
-/// Barrel file for info feature
 export 'presentation/krankmeldung_info_screen.dart';
 export 'presentation/legal_screen.dart';
--- lib/features/substitution/index.dart
+++ lib/features/substitution/index.dart
@@ -1,6 +1,5 @@
 // Copyright Luka Löhr 2025
 
-/// Barrel file for substitution feature
 export 'presentation/substitution_screen.dart';
 export 'application/substitution_provider.dart';
 export 'domain/substitution_models.dart';
