ec466f25d501bb40e537f5ea50cf0589b9c9ae53
luka-loehr <ycrafty6@gmail.com>
2026-01-04 15:41:34 +0100

    Fix all info-level style suggestions

--- lib/features/home/index.dart
+++ lib/features/home/index.dart
@@ -1,5 +1,4 @@
 // Copyright Luka Löhr 2025
 
-/// Barrel file for home feature
 export 'presentation/home_screen.dart';
 export 'presentation/drawer_modal.dart';
--- lib/features/settings/index.dart
+++ lib/features/settings/index.dart
@@ -1,5 +1,4 @@
 // Copyright Luka Löhr 2025
 
-/// Barrel file for settings feature
 export 'presentation/settings_modal.dart';
 export 'presentation/bug_report_screen.dart';
